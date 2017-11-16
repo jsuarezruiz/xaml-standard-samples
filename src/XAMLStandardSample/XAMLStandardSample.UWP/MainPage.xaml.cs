781b1da220ed6543cd75960addc9f1598eef1e6a
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-11-16 21:39:06 +0100

    Initial commit

---
+++ src/XAMLStandardSample/XAMLStandardSample.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace XAMLStandardSample.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new XAMLStandardSample.App());
+        }
+    }
+}
---
+++ src/XAMLStandardSample/XAMLStandardSample/Views/MainView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XAMLStandardSample.ViewModels;
+
+namespace XAMLStandardSample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
+    {
+        public MainView()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewModel();
+        }
+    }
+}
